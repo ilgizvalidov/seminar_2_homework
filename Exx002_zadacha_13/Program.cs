e964e02cd91da2692b2426fbcb56b5e85278d62a
ilgiz validov <ilgiz.validov@gmail.com>
2022-09-16 20:29:29 +0300

    программа принимает на вход от пользователя  целое положительное число и выдает вторую цифру цчисла. если у числа нет второй цифры вывожит сообщение об этом

---
+++ Exx002_zadacha_13/Program.cs
@@ -0,0 +1,30 @@
+﻿
+void SecondDigit (int Arg) 
+{
+    int Result = (Arg/10)%10; 
+    Console.WriteLine ($" вторая цифра будет {Result}");
+}
+ 
+try 
+{
+   Console.WriteLine ("введите трехзначное целое положительное число ");
+   int Number = Convert.ToInt32(Console.ReadLine ());
+   if (Number < 1000 )
+        if (Number > 99) SecondDigit (Number);
+        else Console.WriteLine("в этом числе ворой цифры нет"); 
+   else 
+   {
+         
+        while (Number > 1000)
+        {
+            Number = Number/10;
+             
+        }
+        SecondDigit (Number);
+   }
+    
+}
+catch
+{
+    Console.WriteLine ("Вы ввели не  число");
+}
