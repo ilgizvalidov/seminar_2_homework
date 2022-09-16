b93d64693c73e3e08cea1d392deacfd0f1cfee35
ilgiz validov <ilgiz.validov@gmail.com>
2022-09-16 19:30:35 +0300

    программа принимает на вход от пользователя трехзначное целое положительное число и выдает вторую цифру цчисла.

---
+++ Exx001_zadach_10/Program.cs
@@ -0,0 +1,20 @@
+﻿void SecondDigit (int Arg) 
+{
+    int Result = (Arg/10)%10; 
+    Console.WriteLine ($" вторая цифра числа {Arg} будет {Result}");
+}
+ 
+
+try 
+{
+   Console.WriteLine ("введите трехзначное целое положительное число ");
+   int Number = Convert.ToInt32(Console.ReadLine ());
+   if (Number < 1000 )
+        if (Number > 99) SecondDigit (Number);
+        else Console.WriteLine("вы ввели число содержащее меньше трех знаков"); 
+   else Console.WriteLine ("вы ввели число собдержащее более трех знаков");
+}
+catch
+{
+    Console.WriteLine ("Вы ввели не  число");
+}
