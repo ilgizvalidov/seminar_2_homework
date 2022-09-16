5f2beb73b0d95026180c3f546f7f2bc6a6f42522
ilgiz validov <ilgiz.validov@gmail.com>
2022-09-16 20:52:28 +0300

    программа принимает на вход цифр дня недели, а на выход выводит сообщение если день выходной или нет

---
+++ Exx003_zadacha_15/Program.cs
@@ -0,0 +1,29 @@
+﻿try 
+{
+ 
+    Console.WriteLine ("Введите цифру от 1 до 7 соответствующую намеру дня недели, где 1 это понедельник, а 7 - воскресенье");
+    int Number = Convert.ToInt32 (Console.ReadLine ());
+switch (Number)
+{
+    case 1: Console.WriteLine ("нет");
+        break;
+    case 2: Console.WriteLine ("нет");
+        break;
+    case 3: Console.WriteLine ("нет"); 
+        break;
+    case 4: Console.WriteLine ("нет");
+        break;
+    case 5: Console.WriteLine ("нет");
+        break;
+    case 6: Console.WriteLine ("да");
+        break;
+    case 7: Console.WriteLine ("lда");
+        break;
+    default: Console.WriteLine ("введите привильное число");
+        break;                       
+}
+} 
+catch 
+{
+    Console.WriteLine ("вы ввели не цифру");
+}
