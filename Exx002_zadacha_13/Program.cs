3fa427f852dfb8cb0f54494d9300590c0cab93a2
ilgiz validov <ilgiz.validov@gmail.com>
2022-09-16 20:34:58 +0300

    программа принимает на вход от пользователя  целое положительное число и выдает третью  цифру числа. если у числа нет третей цифры выводит сообщение об этом

--- Exx002_zadacha_13/Program.cs
+++ Exx002_zadacha_13/Program.cs
@@ -1,17 +1,17 @@
 ﻿
 void SecondDigit (int Arg) 
 {
-    int Result = (Arg/10)%10; 
-    Console.WriteLine ($" вторая цифра будет {Result}");
+    int Result = Arg%10; 
+    Console.WriteLine ($" третья цифра будет {Result}");
 }
  
 try 
 {
-   Console.WriteLine ("введите трехзначное целое положительное число ");
+   Console.WriteLine ("введите  целое положительное число ");
    int Number = Convert.ToInt32(Console.ReadLine ());
    if (Number < 1000 )
         if (Number > 99) SecondDigit (Number);
-        else Console.WriteLine("в этом числе ворой цифры нет"); 
+        else Console.WriteLine("в этом числе второй цифры нет"); 
    else 
    {
          
